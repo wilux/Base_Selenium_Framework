e78c47240df70b9a561e2fc0c5b5dacaf5a56528
Nestor Flores <nestorchoele@gmail.com>
2022-07-28 15:11:11 -0300

    add file upload

--- src/test/java/TestCase/ContinuarPaso7Test.java
+++ src/test/java/TestCase/ContinuarPaso7Test.java
@@ -10,6 +10,8 @@ import org.openqa.selenium.By;
 import org.testng.annotations.Test;
 
 import java.awt.*;
+import java.awt.datatransfer.StringSelection;
+import java.awt.event.KeyEvent;
 
 
 public class ContinuarPaso7Test extends BaseTest {
@@ -38,23 +40,51 @@ public class ContinuarPaso7Test extends BaseTest {
         driver.findElement(By.linkText("Continuar solicitud")).click();
 
         //Identificación Oficial *(Debes Subir el Anverso y Reverso de la Identificación Oficial)
-        driver.findElement(By.xpath("(//div[@data-dztype='identificacionOficial']//div)[1]")).click();
+        driver.findElement(By.xpath("//*[@id='idDropzone']")).click();
 
-        //Comprobante de domicilio
-        driver.findElement(By.xpath("(//div[@data-dztype='comprobanteDeDomicilio']//div)[1]")).click();
+        Thread.sleep ( 1000 );
+        // creating object of Robot class
+        Robot rb = new Robot();
 
-        //Comprobante de ingresos *(Debes Subir 6 Comprobantes de Ingresos)
-        driver.findElement(By.xpath("//div[@data-dztype='asalariado']")).click();
+        // copying File path to Clipboard
+        StringSelection str = new StringSelection( "C:\\Users\\floresnes\\IdeaProjects\\Base_Selenium_Framework\\src\\test\\resources\\test.png");
+        Toolkit.getDefaultToolkit().getSystemClipboard().setContents(str, null);
 
-        //Garantía Automotriz
-        driver.findElement(By.xpath("//div[@data-dztype='garantiasVehiculares']")).click();
+        // press Contol+V for pasting
+        rb.keyPress( KeyEvent.VK_CONTROL);
+        rb.keyPress(KeyEvent.VK_V);
 
-        //Solicitud de crédito
-        driver.findElement(By.xpath("(//div[@data-dztype='solicitudDeCredito']//div)[1]")).click();
+        // release Contol+V for pasting
+        rb.keyRelease(KeyEvent.VK_CONTROL);
+        rb.keyRelease(KeyEvent.VK_V);
 
-        //Fin
-        //Siguiente
-        driver.findElement(By.linkText("Siguiente")).click();
+        // for pressing and releasing Enter
+        rb.keyPress(KeyEvent.VK_ENTER);
+        rb.keyRelease(KeyEvent.VK_ENTER);
+
+        //confirmo documento
+        Thread.sleep ( 4000 );
+        js.executeScript("cargarDocumento(25)");
+//
+//        //Comprobante de domicilio
+//        driver.findElement(By.xpath("(//div[@data-dztype='comprobanteDeDomicilio']//div)[1]")).click();
+//
+//        //confirmo documento
+//        js.executeScript("cargarDocumento2(27)");
+//
+//
+//        //Comprobante de ingresos *(Debes Subir 6 Comprobantes de Ingresos)
+//        driver.findElement(By.xpath("//div[@data-dztype='asalariado']")).click();
+//
+//        //Garantía Automotriz
+//        driver.findElement(By.xpath("//div[@data-dztype='garantiasVehiculares']")).click();
+//
+//        //Solicitud de crédito
+//        driver.findElement(By.xpath("(//div[@data-dztype='solicitudDeCredito']//div)[1]")).click();
+//
+//        //Fin
+//        //Siguiente
+//        driver.findElement(By.linkText("Siguiente")).click();
 
 
         // 7 - Carga Documentos
